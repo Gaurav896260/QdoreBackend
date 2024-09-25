884ff6c124c7908788b9f706f6ad62dce71b2682
gaurav shukla <122368972+Gaurav896260@users.noreply.github.com>
2024-09-25 23:27:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,14 +25,22 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-    allowedHeaders: ['Authorization', 'Content-Type'],
-}));
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin === 'http://localhost:5173') {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+  allowedHeaders: ['Authorization', 'Content-Type'], // Enable this if cookies or authentication tokens are used
+};
+
+app.use(cors(corsOptions));
 
 // Handle preflight requests
-app.options('*', cors());
+app.options('*', cors(corsOptions));
 
 app.use(helmet());
 app.use(xss());
