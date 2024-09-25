bb05b7207688b20a823314ae5003447d26492b21
gaurav shukla <122368972+Gaurav896260@users.noreply.github.com>
2024-09-25 14:24:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,34 @@
-import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { resolve } from 'path';
+// import { defineConfig } from "vite";
+// import wasm from "vite-plugin-wasm";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import { resolve } from 'path';
 
-export default defineConfig({
-  root:'./',
-  plugins: [
-    wasm(),
-    nodePolyfills(),  // Make sure this is being used correctly
-  ],
-  resolve: {
-    alias: {
-      'stream-browserify': resolve(__dirname, 'node_modules/stream-browserify'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: "index.js",
-      output: {
-        format: "esm",
-        globals: {
-          'stream-browserify': 'stream',
-        },
-      },
-    },
-    target: "es2022",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2022",
-    },
-  },
-});
+// export default defineConfig({
+//   root:'./',
+//   plugins: [
+//     wasm(),
+//     nodePolyfills(),  // Make sure this is being used correctly
+//   ],
+//   resolve: {
+//     alias: {
+//       'stream-browserify': resolve(__dirname, 'node_modules/stream-browserify'),
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       input: "index.js",
+//       output: {
+//         format: "esm",
+//         globals: {
+//           'stream-browserify': 'stream',
+//         },
+//       },
+//     },
+//     target: "es2022",
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       target: "es2022",
+//     },
+//   },
+// });
