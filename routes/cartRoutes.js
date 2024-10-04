1f4e7fa19ff01b2699dd4a3ea678cf172bf10348
gaurav shukla <122368972+Gaurav896260@users.noreply.github.com>
2024-10-04 19:39:26 +0530

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getCart, addToCart } from "../controllers/cartController.js";
+import {
+  getCart,
+  addToCart,
+  UpdateItem,
+} from "../controllers/cartController.js";
 import { authenticate } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/cart/:userId", authenticate, getCart);
 router.post("/cart", authenticate, addToCart);
+router.patch("/cart/:userId/update/:productId", authenticate, UpdateItem);
 
 export default router;
