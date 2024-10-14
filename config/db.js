2ad7a1b8fc01195be3b291ed0c2d9c2e9d21dafe
gaurav shukla <122368972+Gaurav896260@users.noreply.github.com>
2024-10-14 21:00:15 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(`successfully connected to MongoDB`);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Set the maximum number of connections in the pool
+      maxPoolSize: 10,
+      // Set the minimum number of connections in the pool
+      minPoolSize: 2,
+      // Set the maximum number of milliseconds a connection can be idle before closing
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
+    console.log(`Successfully connected to MongoDB`);
   } catch (error) {
-    console.error(`Error :${error.message}`);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
